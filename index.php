7e20f90cf17698cdd018aede3744e94a94673c89
Liam Mann <liam@liammann.com>
2012-03-17 15:41:48 +0000

    Version 1 of site

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Simple Photo Gallery - Moodle </title>
+	<link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" /> 
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body id="home">
+
+<header>
+	<a class="logo" href="http://liammann.github.com/Moodle-SimplePhotoGallery/"><img src="img/logo.png" alt="logo" /></a>
+	<a class="mylogo" href="http://liammann.com"><img src="img/liamlogo.png" alt="Liam Mann" /></a>
+</header>
+
+<section>
+	<h2>Description</h2>
+	<p>Add a very simple photo gallery to your Moodle course without have to re-upload your flickr pics. All you have to do is enter the URL of your flickr set and your done.</p>
+	<h2>Features</h2>
+	<ul>
+		<li>Add photo gallery from any public set</li>
+		<li>Display using fancy-box or slideshow</li>
+	</ul>
+	<h3>Coming Soon</h3>
+	<p>Embed straight onto the course page</p>
+</section>
+<div>
+<span><img src="img/example1.png" alt="example1" /></span>
+<span><img src="img/example2.png" alt="example2" /></span>
+</div>
+
+<section class="download">
+    <div class="button">
+	      <a href="https://github.com/liammann/Moodle-SimplePhotoGallery/zipball/master"> Download</a>
+	      <p class="top">Download From GitHub</p>
+	      <p class="bottom">100 KB .zip</p>
+	    </div>
+</section>
+</body>
+</html>
